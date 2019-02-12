2a0421eb566b99d3994d6c3d8dd2541d8a8af2b2
milad mortazavi <milad@cascadetechnologies.com>
2019-02-11 21:04:20 -0800

    finished assignment 2

--- Homeworks/A2/LD.py
+++ Homeworks/A2/LD.py
@@ -1,37 +1,79 @@
 from methods import getData_simple
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
 
 def computeLD(snp):
    
   n,m = snp.shape
 
-  LD = np.zeros_like(snp)
+  LD = np.zeros((m,m))
+  Pval = np.ones((m,m))
 
-  for i in xrange(n):
-    for j in xrange(i+1,n):
+  for i in range(m):
+    for j in range(0,i):
 
       A0s = (snp[:,i]==0)
       As0 = (snp[:,j]==0)
+      A1s = (snp[:,i]==1)
+      As1 = (snp[:,j]==1)
       A00 = np.logical_and(A0s, As0)
 
-      P0s = np.sum(A0s)/float(n)
-      Ps0 = np.sum(As0)/float(n)
-      P00 = np.sum(A00)/float(n)
+      P0s = float(np.sum(A0s))/float(n)
+      Ps0 = float(np.sum(As0))/float(n)
+      P1s = float(np.sum(A1s))/float(n)
+      Ps1 = float(np.sum(As1))/float(n)
+      P00 = float(np.sum(A00))/float(n)
 
-      LD[i,j] = P00 - P0s*Ps0
+      D = P00 - P0s*Ps0
+      if (D>0):
+        denom = min(P0s*Ps1, P1s*Ps0)
+        LD[i,j] = D/denom
+      else: 
+        denom = max(-P0s*Ps0, -P1s*Ps1)
+        LD[i,j] = D/denom
+  
+      Pval[i,j] = 1.0 - stats.chi2.cdf(D*D*n/(P0s*Ps0*P1s*Ps1),1)
+        
 
       #print 'P0s: ' , P0s
       #print 'Ps0: ' , Ps0
       #print 'P00: ' , P00
       #print 'P00 - P0s*Ps0: ' , P00 - P0s*Ps0
-  return LD
+  return LD , Pval
 
 if __name__ == "__main__":
 
   fn = "pop2_mod.txt"
   snp = getData_simple(fn)
-  print snp
-  print snp.shape
+  #print snp
+  #print snp.shape
 
-  LD = computeLD(snp)
-  print LD
+  LD , Pval = computeLD(snp)
+  #LD , Pval = computeLD(snp[:,0:50])
+  #print "LD.shape: ", LD.shape
+  #print(LD[2,1])
+
+  mask = np.zeros_like(LD)
+  mask[np.triu_indices_from(mask)] = True
+  with sns.axes_style("white"):
+    plt.figure()
+    ax = sns.heatmap(LD, mask=mask, vmax=.5, square=True,  cmap="YlGnBu")
+    plt.savefig('LD_heatmap.png')
+  with sns.axes_style("white"):
+    plt.figure()
+    ax = sns.heatmap(Pval, mask=mask, square=True,  cmap="YlGnBu")
+    plt.savefig('Pval_heatmap.png')
+  with sns.axes_style("white"):
+    plt.figure()
+    ax = sns.heatmap(-np.log10(Pval), mask=mask, vmin=0, vmax=3., square=True,  cmap="YlGnBu")
+    plt.savefig('mlogPval_heatmap.png')
+    plt.show()
+
+  #print(Pval)
+  #print(-np.log10(Pval))
+  #plt.figure()
+  #plt.imshow(LD, cmap='hot', interpolation='nearest')
+  #plt.show()
+  #print LD
--- Homeworks/A2/R/P2.R
+++ Homeworks/A2/R/P2.R
@@ -1,81 +1,106 @@
-n = 10
-theta = 40
-#N = 1e6
-N = 1000
-#mu = theta/(4*N)
-mu = 1e-1
-l = 1e6
 
-tree = list(list())
+n = 100             # Sample size
+N0 = 1000000       # Population size
+mu = 1e-8          # number of mutations per generation per bp
+theta = 0.04       # Scaled mutation rate
+l = 10000          # number of base pairs
+alph = 7/(4*N0)
+#alph = 0
 
-for (i in 1:n) {
-  tree[[1]][i] = list(i)
-}
+nloops = 200
+xsi = matrix(0, ncol = (n-1))
 
-# making a random tree
-for (k in n:2) {
-  tn = n+1-k
-  rand2 = sample.int(k,2)
-  tree[[tn+1]] = list()
+for (il in 1:nloops) {
+  gen = 0
+  tree = list(list())
 
-  merge_set = unlist(union(tree[[tn]][rand2[1]], tree[[tn]][rand2[2]]))
-  rem_set = tree[[tn]]
-  rem_set = rem_set[-c(rand2[1],rand2[2])]
-
-  tree[[tn+1]][[1]] = (merge_set)
-  tree[[tn+1]] = c(tree[[tn+1]],(rem_set))
-
-  #print('tn+1: ')
-  #print(tn+1)
-  #print(merge_set)
-  #print(rem_set)
-  #print(tree[[tn+1]])
-}
-
-# find the mutations
-snp = matrix(0,nrow=n,ncol=1)
-
-#n = 10
-#N = 1000
-#mu = 1e-1
-mut_nums = c()
-for (k in n:2) {
-  k_ch_2 = k*(k-1)/2.0
-  Tk = N/(k_ch_2)
-  #print("Tk:")
-  #print(Tk)
-  p_mean = Tk*mu
-  #print('p_mean:')
-  #print(p_mean)
-  rand_vec = rpois(k,p_mean)
-  #print('rand_vec:')
-  #print(rand_vec)
-  mut_nums = c(mut_nums,rand_vec)
-}
-
-mut_sum = sum(mut_nums)
-mut_locs = sample.int(l,mut_sum)
-
-snp = matrix(0,nrow=n,ncol=mut_sum)
-icol = 1
-ibranch = 1
-for (k in n:2) {
-  tn = n+1-k
-  for (i in 1:k) {
-    if (mut_nums[ibranch] >= 1) {
-      for (imu in 1:mut_nums[ibranch]) {
-        snp[c(unlist(tree[[tn]][i])),icol] = 1
-        icol = icol+1
-      }
-    }
-    ibranch = ibranch+1
+  for (i in 1:n) {
+    tree[[1]][i] = list(i)
   }
-  print('icol:')
-  print(icol)
+
+  # Making a random tree
+  for (k in n:2) {
+    tn = n+1-k
+    rand2 = sample.int(k,2)
+    tree[[tn+1]] = list()
+
+    merge_set = unlist(union(tree[[tn]][rand2[1]], tree[[tn]][rand2[2]]))
+    rem_set = tree[[tn]]
+    rem_set = rem_set[-c(rand2[1],rand2[2])]
+
+    tree[[tn+1]][[1]] = (merge_set)
+    tree[[tn+1]] = c(tree[[tn+1]],(rem_set))
+
+    #print('tn+1: ')
+    #print(tn+1)
+    #print(merge_set)
+    #print(rem_set)
+    #print(tree[[tn+1]])
+  }
+
+  # Find the mutations
+  snp = matrix(0,nrow=n,ncol=1)
+  mut_nums = c()
+
+  for (k in n:2) {
+    k_ch_2 = k*(k-1)/2
+    Nt = exp(-alph*gen)*N0
+    Tk = rgeom(1, k_ch_2/(2*Nt))
+    #Tk = N0/(k_ch_2)
+    gen = gen + Tk
+  #  Tk = N/(k_ch_2)
+    #print("Tk:")
+    #print(Tk)
+    p_mean = l*Tk*mu
+    #print('p_mean:')
+    #print(p_mean)
+    rand_vec = rpois(k,p_mean)
+    #print('rand_vec:')
+    #print(rand_vec)
+    mut_nums = c(mut_nums,rand_vec)
+  }
+  
+  # mut_sum is the total number of mutations
+  mut_sum = sum(mut_nums)
+  mut_locs = sample.int(l,mut_sum)
+  
+  snp = matrix(0, nrow = n, ncol = mut_sum)
+  icol = 1
+  ibranch = 1
+  
+  for (k in n:2) {
+    tn = n+1-k
+    for (i in 1:k) {
+      if (mut_nums[ibranch] >= 1) {
+        for (imu in 1:mut_nums[ibranch]) {
+          snp[c(unlist(tree[[tn]][i])),icol] = 1
+          icol = icol+1
+        }
+      }
+      ibranch = ibranch + 1
+    }
+  #  print('icol:')
+  #  print(icol)
+  }
+  
+  colnames(snp) = mut_locs
+  
+  Num = matrix(0, ncol = dim(snp)[2])
+  for (i in 1:dim(snp)[2]) {
+    Num[i] = sum(snp[,i])
+  }
+  
+  for (i in 1:(n-1)) {
+    xsi[i] = xsi[i] + length(which(Num == i))
+  }
+  
 }
 
-colnames(snp) = mut_locs
+xsi = xsi / nloops
+xsi = xsi * c(1:(n-1)) / l
 
-
-
+png("P2_alpha=7.png")
+plot(1:(n-1), xsi, type="b", xlab="i", ylab="i*xsi", main='alpha=7/(4*N)')
+#lines(c(1,(n-1)),c(0.04,0.04), type="l")
+dev.off()
 
